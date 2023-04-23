f52e3398f73bbccf245d0e99080317341a84daa8
saurabh-kd-singh <sksingh5087@gmail.com>
2023-04-23 20:17:59 +0530

    Get Bootcamps in a certain functionality
    Created a router and a controller to find the bootcamps from the database which are in a certain distance from the entered zipcode as a center.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -36,7 +37,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
         if (!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+                return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
         res.status(200).json({success: true, data: bootcamp});
 
@@ -48,7 +49,36 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         if (!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+                return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
         res.status(200).json({success: true, data: {}});
+});
+
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+        const { zipcode, distance } = req.params;
+
+        // Get Latitude / longitude from geocoder
+        const loc = await geocoder.geocode(zipcode);
+        const lat = loc[0].latitude;
+        const lng = loc[0].longitude;
+
+        // Calculate radius using radians
+        // Divide distance by radius of earth
+        // Earth Radius = 3963 miles / 6378 Km
+        const radius = distance / 3963;
+
+        const bootcamps = await Bootcamp.find({
+                location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+        });
+
+        res.status(200).json({
+                success: true,
+                count: bootcamps.length,
+                data: bootcamps
+        });
+
 });
